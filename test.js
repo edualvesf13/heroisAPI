36235daaa83a3d3de2b630e104aaf3a6d7ba08f9
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-08-18 11:52:36 -0300

    Test creation - Manipulating files - CREATE.

--- test.js
+++ test.js
@@ -13,6 +13,10 @@ const DEFAULT_ITEM_CADASTRAR = {
 
 describe('Suite de manipulão de Heróis', () => {
 
+    before( async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    } )
+
     it('Deve pesquisar um herói, usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [resultado] = await database.listar(expected.id)
@@ -20,7 +24,9 @@ describe('Suite de manipulão de Heróis', () => {
     })
     it('Deve cadastrar um herói, usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        //
-        deepEqual(null, expected)
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+
+        deepEqual(actual, expected)
     })
 })
