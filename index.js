3a024ccf961c6a303b250886f8a153ec7a6a96ba
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-08-22 14:40:47 -0300

    Commander - remover.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ async function main() {
         .version('v1')
         .option('-n, --nome [value]', "Nome do Herói")
         .option('-p, --poder [value]', "Poder do Herói")
+        .option('-i, --id [value]', "ID do Herói")
 
         .option('-c, --cadastrar', "Cadastrar um Herói")
         .option('-l, --listar', "Listar um Herói")
+        .option('-r, --remover [value]', "Remove um Herói pelo ID")
 
         .parse(process.argv)
 
@@ -18,6 +20,9 @@ async function main() {
     try {
 
         if (Commander.cadastrar) {
+
+            delete heroi.id
+
             const resultado = await Database.cadastrar(heroi)
             
             if (!resultado) {
@@ -32,6 +37,17 @@ async function main() {
             const resultado = await Database.listar()
             console.log(resultado)
             return
+        }
+
+        if (Commander.remover) {
+            const resultado = await Database.remover(heroi.id)
+
+            if (!resultado) {
+                console.error('Não foi possível remover o Herói')
+                return
+            }
+
+            console.log('Herói removido com sucesso!');
         }
     } catch (error) {
         console.error('DEU RUIM ', error);
