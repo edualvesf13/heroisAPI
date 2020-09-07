8950e2ee217f8cd58686c0af9a2a1ae35bd2c0b1
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-09-07 18:46:14 -0300

    Remove herois with POSTGRES - DELETE.

--- multi-db/src/tests/postgresStrategy.test.js
+++ multi-db/src/tests/postgresStrategy.test.js
@@ -16,6 +16,7 @@ describe('Postgres Strategy', function () {
     this.timeout(Infinity)
     this.beforeAll(async function () {
         await context.connect()
+        await context.delete()
         await context.create(MOCK_HEROI_ATUALIZAR)
     })
 
@@ -46,5 +47,11 @@ describe('Postgres Strategy', function () {
         const [itemAtualizado] = await context.read({ id: itemAtualizar.id })
         assert.deepEqual(result, 1)
         assert.deepEqual(itemAtualizado.nome, novoItem.nome)
+    })
+
+    it('Remover Herói por id', async function () {
+        const [item] = await context.read({})
+        const result = await context.delete(item.id)
+        assert.deepEqual(result, 1)
     })
 })
